02aa06efebe2aee66f52bd1692fe710239a37e0d
Inhji <jjenne@inhji.com>
2014-03-24 20:28:07 +0100

    add browserify bundling, sort out deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp    = require("gulp"),
-    mocha   = require("gulp-mocha"),
-    rename  = require("gulp-rename"),
-	jshint  = require("gulp-jshint"),
-	uglify 	= require("gulp-uglify"),
-	coffee = require("gulp-coffee"),
-	coffeelint = require('gulp-coffeelint'),
-	stylish = require("jshint-stylish");
+var gulp    	= require("gulp"),
+    mocha   	= require("gulp-mocha"),
+    rename  	= require("gulp-rename"),
+	jshint  	= require("gulp-jshint"),
+	uglify 		= require("gulp-uglify"),
+	stylish 	= require("jshint-stylish"),
+	browserify 	= require("browserify"),
+	source 		= require('vinyl-source-stream'),
+	streamify 	= require('gulp-streamify');
 
 gulp.task("test", function () {
 	gulp.src("./test/*-test.js")
@@ -16,22 +17,25 @@ gulp.task("test", function () {
 		}));
 });
 
-gulp.task("minify", function () {
-	gulp.src(["./beta.js"])
-		.pipe(uglify())
-		.pipe(rename("beta.min.js"))
-		.pipe(gulp.dest("./"));
+
+gulp.task('dist', function() {
+  var bundleStream = browserify('./beta.js').bundle();
+
+  bundleStream
+    .pipe(source('./beta.js'))
+    .pipe(streamify(uglify()))
+    .pipe(rename("beta.min.js"))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task("watch", function () {
 	var code = gulp.watch([
 		"./beta.js",
 		"./lib/*.js",
-		"./test/*-test.js"], 
-		["test"]);
+		"./test/*-test.js"
+	], ["init"]);
+	console.log("\nAlways watching..\n");
 });
 
-gulp.task("default", ["watch"]);
-gulp.task("min", ["minify"]);
-
-
+gulp.task("default", ["init", "watch"]);
+gulp.task("init", ["test", "dist"]);

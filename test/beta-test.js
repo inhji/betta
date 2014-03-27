f6271161c477042ba78dc61e85bf5a7d13f2208f
Inhji <jjenne@inhji.com>
2014-03-27 03:01:23 +0100

    remove uneccessary tests

--- test/beta-test.js
+++ test/beta-test.js
@@ -1,8 +1,5 @@
-var requireDir  = require('require-dir');
-var chai 		= require("chai");
-var expect  	= chai.expect;
-var beta 		= require("../beta.js");
-var classes     = requireDir('../lib/classes');
+var expect 		= require("chai").expect;
+var beta 		= require("../dist/beta.min.js");
 
 describe("ßeta", function () {
 	it('should be a function', function () {
@@ -11,6 +8,7 @@ describe("ßeta", function () {
 
 	describe('invocation', function () {
 		describe('generic methods', function () {
+
 			describe('method is()', function () {
 				it('should return string', function () {
 					expect(beta("foo").is()).to.equal("string");
@@ -23,23 +21,9 @@ describe("ßeta", function () {
 				});
 			});
 
-			describe('method isDef()', function () {
-				it('should return true', function () {
-					expect(beta("foo").isDef()).to.equal(true);
-				});
-			});
-
-			describe('method isUndef()', function () {
-				it('should return false', function () {
-					expect(beta("foo").isUndef()).to.equal(false);
-				});
-			});
 		});
 
 		describe('string methods', function () {
-			it('should return an instance of betaString', function () {
-				expect(beta("foo")).to.be.an.instanceof(classes.string);
-			});
 
 			describe('method is()', function () {
 				it('should return string', function () {

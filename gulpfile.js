6c33a680b4f0b7f66e1ee642cf5991633e94abaa
Inhji <jjenne@inhji.com>
2014-04-07 22:31:59 +0200

    clean up project config

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,26 @@
 var gulp        = require("gulp"),
     mocha       = require("gulp-mocha"),
-    rename      = require("gulp-rename"),
     uglify      = require("gulp-uglify"),
-    coffee      = require('gulp-coffee'),
-    coffeelint  = require('gulp-coffeelint');
+    watch       = require("gulp-watch"),
+    grep        = require("gulp-grep-stream"),
 
-gulp.task('coffee', function() {
-  gulp.src('./src/*.coffee')
-    .pipe(coffee({bare: true}))
-    .pipe(uglify())
-    .pipe(rename("beta.min.js"))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('coffeelint', function () {
-    gulp.src("./src/*.coffee")
-        .pipe(coffeelint())
-        .pipe(coffeelint.reporter())
-});
-
-gulp.task("test", function () {
-    gulp.src("./test/*-test.js")
-        .pipe(mocha({
-            ui: "bdd",
-            globals: ["chai"],
-            reporter: "nyan"
-        }));
-});
-
-gulp.task("watch", function () {
-    var watcher = gulp.watch(["./src/*.coffee"], ["coffee", "coffeelint", "test"]);
+gulp.task("watch", function() {
+    gulp.src(["./test/*.js", "./src/*.js"], { read: false })
+        .pipe(watch({ emit: "all" }, function(files) {
+            files
+                .pipe(grep("*-test.js"))
+                .pipe(mocha({
+                    ui: "bdd",
+                    globals: ["chai"],
+                    reporter: "nyan"
+                }))
+                .on("error", function() {
+                    if (!/tests? failed/.test(err.stack)) {
+                        console.log(err.stack);
+                    }
+                });
+        }))
 });
 
 
-gulp.task("default", ["init", "watch"]);
-gulp.task("init", ["test"]);
+gulp.task("default", ["watch"]);

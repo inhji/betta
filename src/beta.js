08469fca0fa2b5881c4d5bf6365f091be458ed2b
inhji <jjenne@inhji.com>
2014-05-13 22:05:57 +0000

    clean up

--- src/beta.js
+++ src/beta.js
@@ -1,5 +1,5 @@
 /* ---------------------------------
-  ___.           __    __          
+  ___            __    __          
   \_ |__   _____/  |__/  |______   
    | __ \_/ __ \   __\   __\__  \  
    | \_\ \  ___/|  |  |  |  / __ \_
@@ -18,163 +18,25 @@
  --------------------------------- */
 
 (function (root) {
-    "use strict";
-    
-    /* ----- Protos ----- */
 
-    var nativeToString  = Object.prototype.toString,
-        nativeSlice     = Array.prototype.slice;
+    var getType = function (v) {
+        return Object.prototype.toString.call(v)
+            .replace(/\[object\s{1}|\]/g, "")
+            .toLowerCase();
+    };
 
-    /* ----- Helpers ----- */
+    var compareToType = function (v, t) {
+        var s = getType(v);
+        return s === t || s.substr(0, 3) === t;
+    };
 
-    var callToString = function (v) {
-            return nativeToString.call(v)
-                .replace(/\[object\s{1}|\]/g, "")
-                .toLowerCase();
-        },
-
-        compareToType = function (v, t) {
-            var s = callToString(v);
-            return s === t || s.substr(0, 3) === t;
-        },
-        
-        isDefined = function(v) {
-            var s = callToString(v);
-            return s !== "null" && s !== "undefined";
-        },
-
-        isUndefined = function (v) {
-            return !isDefined(v);
-        },
-        
-        isSame = function(prev, current) {
-            return prev && current;
-        },
-        
-        isCurrent = function(prev, current) {
-            return (prev === current)? current: false;
-        },
-
-        isEven = function (number) {
-            return number % 2 === 0;
-        },
-
-        round = function(num, dec) {
-            var multi = Math.pow(10, dec);
-            return Math.round(num * multi) / multi;
-        },
-        
-        trim = function(str) {
-            return str.replace(/^\s+|\s+$/g, "")
-        };
-
-    /* ----- Object Class ----- */
-
-    function ObjectClass(args) {
-        var self = ObjectClass;
-
-        self.values = args;
-        
-        self.is = function(type) {
-            if (type) {                        
-                return self.values
-                    .map(function(value) {
-                        return compareToType(value, type)
-                    }).reduce(isSame);
-            }
-            
-            return self.values
-                .map(callToString)
-                .reduce(isCurrent);
-        };
-        
-        self.isDef = function() {
-            return self.values
-                .map(isDefined)
-                .reduce(isSame);
-        };
-
-        self.isUndef = function () {
-            return self.values
-                .map(isUndefined)
-                .reduce(isSame);
+    var betta = function(value, type) {      
+        if (type) {
+            return compareToType(value, type)
         }
         
-        self.done = function() {
-            return (self.values.length === 1)? self.values[0]: self.values;
-        }
-        
-        return self;
-    }
-
-    /* ----- String ----- */
-
-    function StringClass(args) {
-        var self = ObjectClass(args);
-        
-        self.trim = function(str){
-            self.values = self.values.map(trim);
-            
-            return self;
-        }
-
-        return self;
-    }
-
-    /* ----- Function ----- */
-    
-    function FunctionClass(args) {
-        var self = ObjectClass(args);
-        
-        return self;
-    }
-    
-    /* ----- Number ----- */
-    
-    function NumberClass(args){
-        var self = ObjectClass(args);
-
-        self.round = function (dec) {
-            self.values = self.values.map(function (val) {
-                return round(val, dec || 0);
-            });
-            return self;
-        }
-        
-        return self;
-    }
-
-    /* ----- Wrapper ----- */
-
-    function betta() {
-        var args = nativeSlice.call(arguments),
-            type;
-        
-        if(args.length === 0)
-            return false;
-            
-        type = args
-            .map(callToString)
-            .reduce(isSame);
-            
-        if (type === false) {
-            return false;
-        }
-            
-        switch (type) {
-            case "string":
-                return StringClass(args);
-            case "number":
-                return NumberClass(args);
-            case "array":
-                return ArrayClass(args);
-            case "function":
-                return FunctionClass(args);
-            default:
-                return ObjectClass(args);
-        }
-        
-    }
+        return getType(value);
+    };
 
     /* ----- Exports ----- */
 

7e0813620d7d696c7197080d7e5a3af3b5c07ec1
Inhji <jjenne@inhji.com>
2014-05-18 02:04:07 +0200

    simplify stuff

--- src/beta.js
+++ src/beta.js
@@ -17,36 +17,21 @@
   v0.0.1
  --------------------------------- */
 
-(function (root) {
+var getType = function (v) {
+    return Object.prototype.toString.call(v)
+        .replace(/\[object\s{1}|\]/g, "")
+        .toLowerCase();
+};
 
-    var getType = function (v) {
-        return Object.prototype.toString.call(v)
-            .replace(/\[object\s{1}|\]/g, "")
-            .toLowerCase();
-    };
+var compareToType = function (v, t) {
+    var s = getType(v);
+    return s === t || s.substr(0, 3) === t;
+};
 
-    var compareToType = function (v, t) {
-        var s = getType(v);
-        return s === t || s.substr(0, 3) === t;
-    };
-
-    var betta = function(value, type) {      
-        if (type) {
-            return compareToType(value, type)
-        }
-        
-        return getType(value);
-    };
-
-    /* ----- Exports ----- */
-
-    if (typeof exports !== "undefined") {
-        if (typeof module !== "undefined" && module.exports) {
-            exports = module.exports = betta;
-        }
-        exports["\u00df"] = betta;
-    } else {
-        root["\u00df"] = betta;
+module.exports = function(value, type) {      
+    if (type) {
+        return compareToType(value, type)
     }
-
-}(this));
+    
+    return getType(value);
+};
